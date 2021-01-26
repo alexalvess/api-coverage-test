a7fd2b52981484a264da0768eab87fb12164f2f4
Alex Alves <alex.silva@plus-it.com.br>
2021-01-26 16:52:36 -0300

    correção de bug ao realizar a leitura do arquivo correto de report

--- buildAndReleaseTask/utils/directory.ts
+++ buildAndReleaseTask/utils/directory.ts
@@ -4,7 +4,7 @@ export function verifySubDirectory(directory: string, fileName: string): boolean
     return fs.statSync(`${directory}\\${fileName}`).isDirectory();
 }
 
-export function getValidFiles(directory: string, validFiles: string[], extension: string = '.xml'): string[] {
+export function getValidFiles(directory: string, validFiles: string[], extension: string = 'junitReport.xml'): string[] {
     let filesAndSubdirectory: string[] = fs.readdirSync(directory);
     filesAndSubdirectory = filesAndSubdirectory.filter(f => f.endsWith(extension) || verifySubDirectory(directory, f));
     const xmlFiles = filesAndSubdirectory.filter(f => f.endsWith(extension));
